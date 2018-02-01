a8ac8ae6a4b971a2ed72f7a4708bd6c610fc5d6c
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-01-31 17:28:48 -0800

    Through API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,20 @@
 var searchYouTube = (options, callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search/',
+    data: {
+      part: 'snippet',
+      key: options.key || window.YOUTUBE_API_KEY,
+      q: options.query || 'baby elephants',
+      maxResults: options.max || '5',
+      videoEmbeddable: 'true',
+      type: 'video'
+    },
+    success: function(results) {
+      // console.log(results);
+      callback(results.items);
+    },
+  });
 };
 
 window.searchYouTube = searchYouTube;
