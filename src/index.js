58b53f6587b932ecab4f5f36ef0adc82840a141b
josh-alamillo7 <josh.alamillo7@gmail.com>
2018-02-01 10:52:07 -0800

    finished bare minimum

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-ReactDOM.render(<App searchYouTube={searchYouTube} />, document.getElementById('app'));
+ReactDOM.render(<App searchYouTube={_.throttle(searchYouTube, 500)} />, document.getElementById('app'));
 
